cfb503a6e87cd63cfb24a5ac30502a6d1950ea31
H3BCKN <mgonline@mgonline.local>
2020-01-08 19:43:28 +0100

    create posts and capybara tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,20 @@ describe 'navigate' do
 			expect(page).to have_content(/Posts/)
 		end
 	end
+	describe 'creation' do
+		it 'should reach the form page' do
+			visit new_post_path
+			expect(page.status_code).to eq(200)
+		end
+
+		it 'should be able to create new post' do
+			visit new_post_path
+
+			fill_in 'post[date]', with: Date.today
+			fill_in 'post[rationale]', with: "New Rationale"
+
+			click_on "Save"
+			expect(page).to have_content("New Rationale")
+		end 
+	end
 end 
