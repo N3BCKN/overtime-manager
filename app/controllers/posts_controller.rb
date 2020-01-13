7fd1a008e2c52323973587ac882cfe04a0d45eb5
H3BCKN <mgonline@mgonline.local>
2020-01-13 01:08:28 +0100

    update posts form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
-		@post.user = current_user	
+		@post.user = current_user
 
 		if @post.save
 			redirect_to @post, notice: "Post has been created"
