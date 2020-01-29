270da116fbd9f8499d78b74385b53c50beb412f3
H3BCKN <mgonline@mgonline.local>
2020-01-29 05:12:48 +0100

    improve approval buttons

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -5,8 +5,13 @@ class AuditLog < ApplicationRecord
 
   validates_presence_of  :status, :start_date
 
+  before_update :set_end_date, if: :confirmed?
+
   private
   def set_default_date
   	self.start_date ||= Date.today - 6.days
-  end  
+  end
+  def set_end_date
+  	self.end_date = Date.today 
+  end 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,15 @@ class Post < ApplicationRecord
 
 	scope :post_by, -> (user) { where(user_id: user.id)}
 
-	after_save :update_audit_log
+	after_save :update_audit_log, if: :submitted?
+	after_save :un_confirm_audit_log, if: :rejected?
 	private 
 	def update_audit_log
 		audit_log = AuditLog.where(user: self.user, start_date: (self.date - 7.days..self.date)).last
-		audit_log.confirmed!
+		audit_log.confirmed! if audit_log
+	end
+	def un_confirm_audit_log
+		audit_log = AuditLog.where(user: self.user, start_date: (self.date - 7.days..self.date)).last
+		audit_log.pending! if audit_log
 	end
 end
